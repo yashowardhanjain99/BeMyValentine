63317ab6403bfc300c1b55165adedf354370d427
yashowardhanjain99 <yashowardhanjain99@gmail.com>
2026-02-03 22:27:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let messageIndex = 0;
+let noClickCount = 0;
 
 const messages = [
   "No 😒",
@@ -11,15 +12,29 @@ const messages = [
 function handleNoClick() {
   const noButton = document.querySelector(".no-button");
   const yesButton = document.querySelector(".yes-button");
+  const img = document.getElementById("reactionImage");
 
+  // Change No button text
   noButton.textContent = messages[messageIndex];
   messageIndex = (messageIndex + 1) % messages.length;
 
+  // Grow Yes button
   const currentSize = parseFloat(
     window.getComputedStyle(yesButton).fontSize
   );
-
   yesButton.style.fontSize = `${currentSize * 1.5}px`;
+
+  // Increase No click count
+  noClickCount++;
+
+  // Image switching (4 images)
+  if (noClickCount === 1) {
+    img.src = "img2.jpg";
+  } else if (noClickCount === 2) {
+    img.src = "img3.jpg";
+  } else if (noClickCount >= 3) {
+    img.src = "img4.jpg";
+  }
 }
 
 function handleYesClick() {
